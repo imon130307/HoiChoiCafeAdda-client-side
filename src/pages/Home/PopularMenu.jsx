2ccb18508df9b7893625233e929eb3273ee9890a
SM-IMON <imon.dvp@gmail.com>
2025-10-06 10:33:16 +0600

    The popular menu is completed

--- src/pages/Home/PopularMenu.jsx
+++ src/pages/Home/PopularMenu.jsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router";
 import SectionTitle from "../../components/SectionTitle/SectionTitle";
 import { useEffect, useState } from "react";
+import MenuItem from "../shared/MenuItem/MenuItem";
 
 const PopularMenu = () => {
   const [menu, setMenu] = useState([]);
@@ -21,32 +22,8 @@ const PopularMenu = () => {
         heading={"FROM OUR MENU"}
       ></SectionTitle>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
-        {menu?.map((item, index) => (
-          <div
-            key={index}
-            className="flex flex-col items-center sm:flex sm:flex-row md:h-[104px] sm:items-start justify-between  rounded-lg hover:scale-105 transition-all duration-300 gap-4 md:gap-8"
-          >
-            <div className="rounded-bl-none rounded-full h-40 w-40 sm:h-full sm:w-24 md:h-full md:w-40 bg-[#D9D9D9] sm:rounded-full  sm:rounded-tl-none overflow-hidden ">
-              <img
-                src={item?.image || item?.imageUrls[0]}
-                alt=""
-                className="h-40 w-40 sm:h-full sm:w-24 md:h-full md:w-40 object-cover"
-              />
-            </div>
-            <div className="flex flex-col sm:flex sm:flex-row sm:justify-between md:justify-start ">
-              <div>
-                <h3 className="text-lg font-semibold new_heading_font text-center sm:text-start">
-                  {item?.name}
-                </h3>
-                <p className="text-gray-600 text-sm text-center sm:text-start">
-                  {item?.recipe.slice(0, 100) + ".."}
-                </p>
-              </div>
-            </div>
-            <div className="text-xl font-bold text-[#BB8506] text-center sm:text-start">
-              ${item?.price}
-            </div>
-          </div>
+        {menu?.map((menuitem, index) => (
+          <MenuItem item={menuitem} key={index} />
         ))}
       </div>
       <div className="text-center mt-8">
---
+++ src/pages/shared/MenuItem/MenuItem.jsx
@@ -0,0 +1,28 @@
+const MenuItem = ({ item }) => {
+  return (
+    <div className="flex flex-col items-center sm:flex sm:flex-row md:h-[104px] sm:items-start justify-between  rounded-lg hover:scale-105 transition-all duration-300 gap-4 md:gap-8">
+      <div className="rounded-bl-none rounded-full h-40 w-40 sm:h-full sm:w-24 md:h-full md:w-40 bg-[#D9D9D9] sm:rounded-full  sm:rounded-tl-none overflow-hidden ">
+        <img
+          src={item?.image || item?.imageUrls[0]}
+          alt=""
+          className="h-40 w-40 sm:h-full sm:w-24 md:h-full md:w-40 object-cover"
+        />
+      </div>
+      <div className="flex flex-col sm:flex sm:flex-row sm:justify-between md:justify-start ">
+        <div>
+          <h3 className="text-lg font-semibold new_heading_font text-center sm:text-start">
+            {item?.name}
+          </h3>
+          <p className="text-gray-600 text-sm text-center sm:text-start">
+            {item?.recipe.slice(0, 100) + ".."}
+          </p>
+        </div>
+      </div>
+      <div className="text-xl font-bold text-[#BB8506] text-center sm:text-start">
+        ${item?.price}
+      </div>
+    </div>
+  );
+};
+
+export default MenuItem;
